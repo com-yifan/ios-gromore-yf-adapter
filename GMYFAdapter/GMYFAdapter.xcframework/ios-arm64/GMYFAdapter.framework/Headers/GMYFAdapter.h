15bd60177072465242a1d9bc243633c212440263
Erik <iosre-erik@MacBook-Pro.local>
2026-04-20 13:56:22 +0800

    build：6.1.0.0

---
+++ GMYFAdapter/GMYFAdapter.xcframework/ios-arm64/GMYFAdapter.framework/Headers/GMYFAdapter.h
@@ -0,0 +1,19 @@
+//
+//  GMYFAdapter.h
+//  GMYFAdapter
+//
+//  Created by Erik on 2026/4/15.
+//  Copyright © 2026 bytedance. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GMYFAdapter.
+FOUNDATION_EXPORT double GMYFAdapterVersionNumber;
+
+//! Project version string for GMYFAdapter.
+FOUNDATION_EXPORT const unsigned char GMYFAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GMYFAdapter/PublicHeader.h>
+
+
